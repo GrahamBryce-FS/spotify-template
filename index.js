e45db9e8a71373ee7971cc7ce857683b5f47c787
Bryce Graham <bgraham1@student.fullsail.edu>
2023-06-29 18:06:54 +0900

    added dotenv

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express=require('express')
+const app = express()
+
+console.log("-----/Env Vars------");
+console.log(process.env) 
+console.log("-----/Env Vars------");
+app.listen(3001)
